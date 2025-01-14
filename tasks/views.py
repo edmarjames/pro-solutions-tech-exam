12b33c9608717078d1212b7274623bbd473886fc
edmar.james <edmarjamesbautista@gmail.com>
2025-01-14 19:42:55 +0800

    Override get_queryset method to implement filtering of task per completed status

--- tasks/views.py
+++ tasks/views.py
@@ -7,3 +7,17 @@ from .serializers import TaskSerializer
 class TaskViewSet(ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        status = self.request.query_params.get("status", None)
+        status = status.lower() if status else None
+        today = localdate()
+
+        if status == "incoming":
+            return queryset.filter(due_date__gt=today)
+        elif status == "today":
+            return queryset.filter(due_date=today)
+        elif status == "overdue":
+            return queryset.filter(due_date__lt=today)
+        return queryset

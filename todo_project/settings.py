d56a8a49dd13dd7b49de25ea3ac30b0c3c94124a
edmar.james <edmarjamesbautista@gmail.com>
2025-01-14 21:46:00 +0800

    Remove comment on settings.py

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -80,10 +80,6 @@ WSGI_APPLICATION = 'todo_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.environ.get('DB_NAME'),

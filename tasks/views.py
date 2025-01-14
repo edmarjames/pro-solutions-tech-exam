212ae091558f4bbf6adda8aeca01fd3174a6dd1b
edmar.james <edmarjamesbautista@gmail.com>
2025-01-14 19:53:14 +0800

    Modify get_queryset to handle invalid status

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.exceptions import ValidationError
 from django.utils.timezone import localdate
 from .models import Task
 from .serializers import TaskSerializer
@@ -11,13 +12,19 @@ class TaskViewSet(ModelViewSet):
     def get_queryset(self):
         queryset = super().get_queryset()
         status = self.request.query_params.get("status", None)
-        status = status.lower() if status else None
         today = localdate()
 
-        if status == "incoming":
-            return queryset.filter(due_date__gt=today)
-        elif status == "today":
-            return queryset.filter(due_date=today)
-        elif status == "overdue":
-            return queryset.filter(due_date__lt=today)
+        if status:
+            status = status.lower()
+            if status == "incoming":
+                return queryset.filter(due_date__gt=today)
+            elif status == "today":
+                return queryset.filter(due_date=today)
+            elif status == "overdue":
+                return queryset.filter(due_date__lt=today)
+            else:
+                raise ValidationError(
+                    {"status": "Invalid value for status. Accepted values are 'incoming', 'today', or 'overdue'."}
+                )
+
         return queryset
--- todo_project/urls.py
+++ todo_project/urls.py
@@ -15,13 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
 
-router = routers.DefaultRouter()
-
-urlpatterns = router.urls
-
-urlpatterns += [
+urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tasks.urls'))
 ]
